e9c7d36da0cb324643a1a688996fdff4dc762e29
SoufianeMab <smabrouk801@gmail.com>
2022-02-24 22:58:45 +0100

    create mode and route and file test

---
+++ routes/commentaires.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var router = express.Router();
+const {PrismaClient} = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+// route select all commentaire
+router.get('/commentaires',async function(req, res, next) {
+  const {take} = req.params.take ;
+  const {skip} = req.params.skip ;
+  const commentaires = await prisma.commentaire.findMany({take:+take,skip:+skip});
+  if (!commentaires) res.status(404).send("Not Found")
+  res.send(commentaires);
+});
+
+// route select unique commentaire
+router.get('/commentaire/:id',async function(req, res, next) {
+  const {id} = req.params.id ;
+  const commentaire = await prisma.commentaire.findUnique({where: {id: +id}});
+  if (!commentaire) res.status(404).send("Not Found")
+  res.send(commentaire);
+});
+
+// route add commentaire
+router.post('/commentaire',async function(req, res, next) {
+  const data = req.body
+  const commentaire = await prisma.commentaire.create({data});
+  res.send(commentaire);
+});
+
+// route update commentaire
+router.patch('/commentaire',async function(req, res, next) {
+  const data = req.body
+  const commentaire = await prisma.commentaire.update({where:{di: data.id}, data});
+  res.send(commentaire);
+});
+
+// route remove commentaire
+router.delete('/commentaire/:id',async function(req, res, next) {
+  const {id} = req.params.id;
+  const commentaire = await prisma.commentaire.delete({where:{id: +id}});
+  res.send(commentaire);
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,44 @@
 var express = require('express');
 var router = express.Router();
+const {PrismaClient} = require('@prisma/client')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const prisma = new PrismaClient()
+
+// route select all user
+router.get('/users',async function(req, res, next) {
+  const {take} = req.params.take ;
+  const {skip} = req.params.skip ;
+  const users = await prisma.user.findMany({take:+take,skip:+skip});
+  res.send(users);
+});
+
+// route select unique user
+router.get('/user/:id',async function(req, res, next) {
+  const {id} = req.params.id ;
+  const user = await prisma.user.findUnique({where: {id: +id}});
+  if (!user) res.status(404).send("Not Found")
+  res.send(user);
+});
+
+// route add user
+router.post('/user',async function(req, res, next) {
+  const data = req.body
+  const user = await prisma.user.create({data});
+  res.send(user);
+});
+
+// route update user
+router.patch('/user',async function(req, res, next) {
+  const data = req.body
+  const user = await prisma.user.update({where:{di: data.id}, data});
+  res.send(user);
+});
+
+// route remove user
+router.delete('/user/:id',async function(req, res, next) {
+  const {id} = req.params.id;
+  const user = await prisma.user.delete({where:{id: +id}});
+  res.send(user);
 });
 
 module.exports = router;
